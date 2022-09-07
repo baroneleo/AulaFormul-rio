291a2c3f065dc95d7a58cd48060c27e062841d3f
Leonardo de Lucena Barone <leonardo.barone1996@gmail.com>
2022-09-07 13:14:04 -0300

    Add files

--- formulario.py
+++ formulario.py
@@ -27,7 +27,7 @@ def gravar():
     if nome and cpf and endereco and senha: # verifica se o valor de nenhum for nulo
         conn = mysql.connect() # Estabelece a conexão com mysql
         cursor = conn.cursor() # Cria um cursor de uma sessão de comunicação com o banco
-        cursor.execute('insert into dados_user (user_nome, user_cpf, user_endereco, user_password) VALUES (%s, %s, %s, %s)', (nome, cpf, endereco, senha)) # Cria o comando "insert" pra inserir dados no banco" referenciando os dados com as variáveis (%s é passar parametro do tipo string)
+        cursor.execute('insert into tbl_user (user_nome, user_cpf, user_endereco, user_password) VALUES (%s, %s, %s, %s)', (nome, cpf, endereco, senha)) # Cria o comando "insert" pra inserir dados no banco" referenciando os dados com as variáveis (%s é passar parametro do tipo string)
         conn.commit() # Executa o comando
     return render_template('formulario.html') # Faz a volta da comunicação
 
@@ -37,7 +37,7 @@ def gravar():
 def listar():
     conn = mysql.connect() # Estabelece a conexão com o banco de dados
     cursor = conn.cursor() # Cria a sessão por meio de um cursor
-    conn.execute('select user_nome, user_cpf, user_endereco from dados_user') # Cria o comando "select" para realizar busca de dados na tabela informada -> (dados_user)
+    conn.execute('select user_nome, user_cpf, user_endereco, password from tbl_user') # Cria o comando "select" para realizar busca de dados na tabela informada -> (dados_user)
     data = cursor.fetchall() # Faz o comando "fetchall" para fazer a recuperação de dados / Atribuindo o resultado pra variável "data"
     conn.commit() # Executa o comando
     return render_template('lista.html', datas = data) # Retorna pro arquivo chamado "lista.html" na pasta de templates / com a variável data com o nome datas que é o nome que ela está sendo referenciado pelo jinja no arquivo lista.html
